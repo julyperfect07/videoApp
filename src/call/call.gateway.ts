44d4f5899abaedc13315037b3e76b4b7c4e64127
julyperfect07 <abdallah.july05@gmail.com>
2026-06-06 17:05:04 +0300

    handle the disconnect

--- src/call/call.gateway.ts
+++ src/call/call.gateway.ts
@@ -1,10 +1,12 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 interface MessageBodyData {
   roomId: string;
@@ -12,14 +14,30 @@ interface MessageBodyData {
 }
 
 @WebSocketGateway()
-export class CallGateway {
+export class CallGateway implements OnGatewayDisconnect {
+  @WebSocketServer()
+  server!: Server;
+  private activeUsers = new Map<string, MessageBodyData>();
+
   @SubscribeMessage('join-room')
   handleJoinRoom(
     @ConnectedSocket() client: Socket,
     @MessageBody() data: MessageBodyData,
   ) {
     const { roomId, peerId } = data;
+
     client.join(roomId);
     client.to(roomId).emit('user-connected', { peerId });
+    this.activeUsers.set(client.id, data);
+  }
+
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    const userData = this.activeUsers.get(client.id);
+
+    if (userData) {
+      const { roomId, peerId } = userData;
+      this.activeUsers.delete(client.id);
+      this.server.to(roomId).emit('user-disconnected', { peerId });
+    }
   }
 }
